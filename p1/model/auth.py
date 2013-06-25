b75a3cd29e7d3f648bfb31df16ecd4ebde49d842
pjo <pjo@ws>
2013-06-25 15:29:14 +0200

    bllla

---
+++ p1/model/auth.py
@@ -0,0 +1,15 @@
+import hashlib
+
+
+def auth(cursor, login, password):
+    cursor.execute(
+        "SELECT id, login, password, imie, nazwisko FROM users \
+        WHERE login=%s and password=%s",
+        (login, hash_pass(password))
+       )
+    return cursor.fetchone() if cursor.rowcount else None
+
+
+def hash_pass(password):
+    SOL_DO_HASEL = "123sdf45"
+    return hashlib.sha224(SOL_DO_HASEL + password).hexdigest()
