15d092c057dc2691b4d0705ca46244b7e0e6074a
pjo <pjo@ws>
2013-06-26 17:08:07 +0200

    i

---
+++ p1/model/schema/schema.py
@@ -0,0 +1,30 @@
+import formencode
+
+
+class Valid(formencode.Schema):
+    allow_extra_fields = True
+    filter_extra_fields = True
+    login = formencode.validators.String(not_empty=True)
+    password = formencode.validators.String(not_empty=True)
+
+
+class ValidAdmin(formencode.Schema):
+    allow_extra_fields = True
+    filter_extra_fields = True
+    login_admin = formencode.validators.String(not_empty=True)
+    password_admin = formencode.validators.String(not_empty=True)
+
+
+class AddUser(formencode.Schema):
+    allow_extra_fields = True
+    filter_extra_fields = True
+    login = formencode.All(
+        formencode.validators.MinLength(4, not_empty=True),
+        formencode.validators.MaxLength(8)
+        )
+#    password = formencode.validators.MinLenght(8, not_empty=True)
+    password_c = formencode.validators.String()
+    chained_validatore = [formencode.validators.FieldsMatch('password',
+        'password_c')]
+    name = formencode.validators.String(not_empty=True)
+    surname = formencode.validators.String(not_empty=True)
